dab329dac25c31cc107944e1bbf08ac89ddec651
Andrew <diver.vlz@gmail.com>
2020-12-20 14:21:47 +0300

    Rus-Fin helper
    Use for sheme select

---
+++ EasySelect/js/foo.js
@@ -0,0 +1,56 @@
+$(function(){
+	$("#subscribe-field-sx1").on("click", function(){
+		$("#BackSide-sx1").toggle();
+	});
+	$("#numbers-field-sx1").on("click", function(){
+		$("#Sign-sx1").toggle();
+	});
+	
+	$("#subscribe-field-sx2").on("click", function(){
+		$("#BackSide-sx2").toggle();
+	});
+	$("#numbers-field-sx2").on("click", function(){
+		$("#Sign-sx2").toggle();
+	});
+
+	$("#subscribe-field-sx4").on("click", function(){
+		$("#BackSide-sx4").toggle();
+	});
+	$("#numbers-field-sx4").on("click", function(){
+		$("#Sign-sx4").toggle();
+	});
+	
+	$("#subscribe-field-sxb").on("click", function(){
+		$("#BackSide-sxb").toggle();
+	});
+	$("#numbers-field-sxb").on("click", function(){
+		$("#Sign-sxb").toggle();
+	});
+
+	$("#sech10").on("mouseover", function(){
+		$("#Main_b_3450_3452").toggle();
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+/* 		$("#Branch_3450_3452 rect").css({"stroke":"red"});
+		$("#Branch_3450_3452 line").css({"stroke":"red"});
+		console.log("cj");
+ */
+
+/* 	$("#RemChOBN").on("click", function(){
+		$(".RemChOBN--ovwerlay").removeClass("hiddenPic");
+		$(".RemLenKal--ovwerlay").addClass("hiddenPic");
+		$(".RemUzStPsGRES--ovwerlay").addClass("hiddenPic");
+	});
+ */
