65e7b5074b39cfe8c78d96b86f2832e9ac9c4fa2
Andrew <diver.vlz@gmail.com>
2022-08-04 00:16:12 +0300

    Data update

--- Exam/js/script.js
+++ Exam/js/script.js
@@ -5,6 +5,8 @@ const AOPO = document.getElementById('AOPO')
 const AOPO_layer = document.getElementById('Layer2')
 const UPASK = document.getElementById('UPASK')
 const UPASK_layer = document.getElementById('Layer3')
+const YV = document.getElementById('YV')
+const YV_layer = document.getElementById('Layer4')
 
 const panzoom = Panzoom(element, {
     maxScale: 10,
@@ -20,4 +22,20 @@ AOPO.addEventListener('click', () => {
 })
 UPASK.addEventListener('click', () => {
   UPASK_layer.classList.toggle('hidden')
- })
+})
+YV.addEventListener('click', () => {
+  YV_layer.classList.toggle('hidden')
+})
+
+const st22 = document.querySelectorAll('.st22')
+console.log(st22)
+
+const showName = (event) => {
+	let el = event.target;
+	console.log(el);
+}
+
+st22.forEach(pcus_elem => {
+  pcus_elem.addEventListener('mouseover', showName)
+})
+
