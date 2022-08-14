d0227060faaf90262b85f2b978f528ef92ecf899
Andrew <diver.vlz@gmail.com>
2022-08-15 00:06:47 +0300

    APC_update

--- Exam/js/script.js
+++ Exam/js/script.js
@@ -9,19 +9,20 @@ const YV = document.getElementById('YV')
 const YV_layer = document.getElementById('Layer4')
 const ETL = document.getElementById('ETL')
 const ETL_layer = document.getElementById('Layer5')
+const APC = document.getElementById('APC')
+// const APC_layer = document.getElementById('Layer5')
 
 const panzoom = Panzoom(element, {
-    maxScale: 10,
+    maxScale: 12,
     minScale: 1,
 });
 
-if (panzoom.Scale == 5) {
-  console.log('KL')
-}
 
 const parent = element.parentElement
 parent.addEventListener('wheel', panzoom.zoomWithWheel);
 reset.addEventListener('click', panzoom.reset)
+
+
 AOPO.addEventListener('click', () => {
  AOPO_layer.classList.toggle('st2')
 })
@@ -35,15 +36,18 @@ ETL.addEventListener('click', () => {
   ETL_layer.classList.toggle('st2')
 })
 
-const st22 = document.querySelectorAll('.st22')
-console.log(st22)
 
 const showName = (event) => {
 	let el = event.target;
 	console.log(el);
 }
 
-st22.forEach(pcus_elem => {
-  pcus_elem.addEventListener('mouseover', showName)
-})
 
+
+let aopo_tihlit = document.getElementById('infoBox')
+
+
+
+APC.addEventListener('click', () => {
+  aopo_tihlit.classList.toggle('hidden')
+});
